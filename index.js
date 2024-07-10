708f331f68aedf354a6f7dd7771c5e739741ffa2
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-07-10 11:32:27 -0400

    generated random URL for joining game room

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 function buttonTwoAction() {
 	let apiResult = getAPIResult();
-   
 }
 
 function getAPIResult() {
 	return 'resultPlaceholder';
+}
+
+function buttonOneAction() {
+	window.location.href = generateRoomURL();
+}
+
+function generateRoomURL() {
+	roomID = generateRoomID();
+	return "https://colonist.io/#" + roomID;
+}
+
+function generateRoomID(numChars = 4) {
+ return Math.random().toString(36).substring(2,numChars+2);
 }
