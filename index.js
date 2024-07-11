3cde279cca9b9d681e38cf2d2f85a544becf0575
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-07-11 18:44:51 -0400

    Added text spread on hover

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ function generateRoomURL() {
 function generateRoomID(numChars = 4) {
  return Math.random().toString(36).substring(2,numChars+2);
 }
+
+function buttonMouseOver(e) {
+	const titleText = e.querySelector('span.btn-title');
+	const textContainer = e.querySelector('.btn-text-top');
+
+	// increase title spacing animation on hover
+	titleText.style.letterSpacing = '0.2em';
+	e.style.textShadow = "0px 0px 0.1rem black";
+}
+
+function buttonMouseOut(e) {
+	const titleText = e.querySelector('span.btn-title');
+	const textContainer = e.querySelector('.btn-text-top');
+
+	titleText.style.letterSpacing = '0.1em';
+	e.style.textShadow = "none";
+}
