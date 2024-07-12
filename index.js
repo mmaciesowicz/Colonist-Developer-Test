cc0e5eb8d2f5bd092cc1aa0b1272235b7114e49e
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-07-12 12:21:01 -0400

    letter spacing hover animation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var spans = document.querySelectorAll('span.btn-title');
+var titles = document.querySelectorAll('.btn-text-top');
+const letterSpacingDiff = 0.15;
+
 async function buttonTwoAction() {
 	let apiResult = await getAPIResult();
 	console.log(apiResult);
@@ -42,15 +46,30 @@ function buttonMouseOver(e) {
 	const titleText = e.querySelector('span.btn-title');
 	const textContainer = e.querySelector('.btn-text-top');
 
-	// increase title spacing animation on hover
-	titleText.style.letterSpacing = '0.2em';
 	e.style.textShadow = "0px 0px 0.1rem black";
+	marginExtraSpace(-1,letterSpacingDiff, Array.from(e));
 }
 
 function buttonMouseOut(e) {
 	const titleText = e.querySelector('span.btn-title');
 	const textContainer = e.querySelector('.btn-text-top');
 
-	titleText.style.letterSpacing = '0.1em';
 	e.style.textShadow = "none";
+	marginExtraSpace(1,letterSpacingDiff, Array.from(e));
 }
+
+// Adds/Subtracts the margin difference from when text lettering is expanded to normal
+// sign is either 1/-1 and marginDifference is a number
+// spans is an array of the title(s) we want to change
+function marginExtraSpace(sign, marginDifference, spans) {
+	const numLetters = Array.from(spans).map(span => span.innerText.length);
+	var extraSpace = [];
+	// Total extra space = Letter spacing difference x (number of characters - 1)
+	for (let i=0; i<spans.length; i++) {
+		extraSpace[i] = (marginDifference * (numLetters[i] - 1))/2;
+		titles[i].style.marginLeft = String(sign * extraSpace[i]) + "em";
+		titles[i].style.marginRight =String(sign * extraSpace[i]) + "em";
+	}
+}
+
+marginExtraSpace(1,letterSpacingDiff, spans);
