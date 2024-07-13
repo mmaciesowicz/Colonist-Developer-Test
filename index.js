2e0ad80b9503e203027f6f8963f8f9effa22eead
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-07-13 14:15:45 -0400

    made button hover effect active only if both buttons can handle

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var spans = document.querySelectorAll('span.btn-title');
-var titles = document.querySelectorAll('.btn-text-top');
 var btns = document.querySelectorAll('.btn');
-const letterSpacingDiff = 0.15;
-const letterSpacingDiffPix = 3;
+var btnTitleDiv = document.querySelector('.btn-title');
+var btnTitleStyle = window.getComputedStyle(btnTitleDiv);
+var defaultLetterSpacing = btnTitleStyle.getPropertyValue('letter-spacing');
+const hoverLetterSpacing = "6.5px";
+const letterSpacingDiffPix = Math.abs(parseFloat(hoverLetterSpacing) - parseFloat(defaultLetterSpacing));
+var buttonHoverOn;
 
 async function buttonTwoAction() {
 	let apiResult = await getAPIResult();
@@ -46,36 +48,65 @@ function generateRoomID(numChars = 4) {
 
 function buttonMouseOver(e) {
 	const titleText = e.querySelector('span.btn-title');
-	const textContainer = e.querySelector('.btn-text-top');
 
-	const numLetters =  titleText.innerText.length;
-	extraSpace = (letterSpacingDiffPix * (numLetters));
-	// console.log(numLetters);
-	// console.log(extraSpace);
+	// const numLetters =  titleText.innerText.length;
+	// extraSpace = (letterSpacingDiffPix * (numLetters));
 
-	// console.log(titleText.offsetWidth);
-	// console.log(e.offsetWidth);
-	const bodyPaddingTotal = parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-right'));
+	// const bodyPaddingTotal = parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-right'));
 	
-	// console.log(titleText.offsetWidth + extraSpace + bodyPaddingTotal/2);
-	if (Math.ceil(titleText.offsetWidth) + extraSpace <= Math.floor(e.offsetWidth) && 
-	Math.ceil(titleText.offsetWidth) + extraSpace < screen.width/2 -bodyPaddingTotal) { /* Ensure the increased text does not fill up more than half of the screen so other button stays the same size */
-		titleText.style.letterSpacing = "6.5px";
+	// if (Math.ceil(titleText.offsetWidth) + extraSpace <= Math.floor(e.offsetWidth) 
+	// 	&& Math.ceil(titleText.offsetWidth) + extraSpace < screen.width/2 -bodyPaddingTotal /* Ensure the increased text does not fill up more than half of the screen so other button stays the same size */
+	// 	&& buttonHoverOn) {
+	// 	titleText.style.letterSpacing = hoverLetterSpacing;
+	// }
+	// else {
+	// 	buttonHoverOn = false;
+	// }
+	if (buttonHoverOn) {
+		titleText.style.letterSpacing = hoverLetterSpacing;
 	}
 }
 
 function buttonMouseOut(e) {
 	const titleText = e.querySelector('span.btn-title');
-	const textContainer = e.querySelector('.btn-text-top');
 
-	const numLetters =  titleText.innerText.length;
-	extraSpace = (letterSpacingDiffPix * (numLetters));
-	// console.log(numLetters);
-	// console.log(extraSpace);
+	// const numLetters =  titleText.innerText.length;
+	// extraSpace = (letterSpacingDiffPix * (numLetters));
 
-	// console.log(titleText.offsetWidth);
-	// console.log(e.offsetWidth);
-	if (Math.ceil(titleText.offsetWidth) - extraSpace <= Math.floor(e.offsetWidth)) {
-		titleText.style.letterSpacing = "3.5px";
+	// if (Math.ceil(titleText.offsetWidth) - extraSpace <= Math.floor(e.offsetWidth)) {
+	// 	titleText.style.letterSpacing = "3.5px";
+	// }
+	if (titleText.style.letterSpacing === hoverLetterSpacing) {
+		titleText.style.letterSpacing = defaultLetterSpacing;
 	}
 }
+
+// Function that determines whether the letter-spacing effect should take place on hover
+function setupHoverEffect() {
+	const bodyPaddingTotal = parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-right'));
+		
+	for (let i=0; i<btns.length; i++) {
+		const titleText = btns[i].querySelector('span.btn-title');
+		const numLetters = titleText.innerText.length;
+		extraSpace = (letterSpacingDiffPix * (numLetters));
+	
+
+		if (Math.ceil(titleText.offsetWidth) + extraSpace <= Math.floor(btns[i].offsetWidth) /* Ensure the increased lettering-space stays on the same line(s) (and does not switch from one line to two or vice versa) */
+			&& Math.ceil(titleText.offsetWidth) + extraSpace < screen.width/2 -bodyPaddingTotal) { /* Ensure the increased lettering-space does not fill up more than half of the screen so other button stays the same size */
+			buttonHoverOn = true;
+		}
+		else {
+			buttonHoverOn = false;
+			break;
+		}
+	}
+}
+
+// Update letter-spacing effect capabilities
+window.addEventListener("resize", () => {
+	setupHoverEffect();
+});
+
+window.onload = function() {
+	setupHoverEffect();
+};
