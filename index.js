00c815915fe19703c6d719945b948ee9fc27c686
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-07-21 16:11:49 -0400

    changed javascript functions to object oriented classes

--- index.js
+++ index.js
@@ -1,92 +1,154 @@
-var btns = document.querySelectorAll('.btn');
-var btnTitleDiv = document.querySelector('.btn-title');
-var btnTitleStyle = window.getComputedStyle(btnTitleDiv);
-var defaultLetterSpacing = btnTitleStyle.getPropertyValue('letter-spacing');
-const hoverLetterSpacing = "6.5px";
-const letterSpacingDiffPix = Math.abs(parseFloat(hoverLetterSpacing) - parseFloat(defaultLetterSpacing));
-var buttonHoverOn;
+const bodyPaddingTotal = parseFloat(getBodyPropertyValue('padding-left')) + parseFloat(getBodyPropertyValue('padding-right'));
 
-async function buttonTwoAction() {
-	let apiResult = await getAPIResult();
-	console.log(apiResult);
-	let ids = apiResult.slice(0, 5).map(item => item.id);
-	console.log("First 5 room ids: " + ids);
+function getBodyPropertyValue(propertyValue) {
+	return window.getComputedStyle(document.body).getPropertyValue(propertyValue);
 }
 
-async function getAPIResult() {
-	let apiResult = await sendAPIRequest().then(data => {
-		return data;
-	});
-	return apiResult.games;
-}
+class ColonistButton {
+	constructor(buttonObject, defaultLetterSpacing = "3.5px") {
+		this.buttonObject = buttonObject;
+		this.titleText = this.buttonObject.querySelector('span.btn-title');
+		this.numLetters = this.titleText.innerText.length;
+		this.defaultLetterSpacing = defaultLetterSpacing;
+	}
+	buttonAction() {
+        throw new Error("This method should be overridden by subclasses");
+    }
 
-async function sendAPIRequest() {
-	const url = 'https://colonist.io/api/game-list';
-
-	return await fetch(url)
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Error retrieving response from server.');
-			}
-			return response.json(); // This returns a promise
-		})
-		.catch(error => console.error('Error:', error));
-}
-
-function buttonOneAction() {
-	window.location.href = generateRoomURL();
-}
-
-function generateRoomURL() {
-	roomID = generateRoomID();
-	return "https://colonist.io/#" + roomID;
-}
-
-function generateRoomID(numChars = 4) {
-	return Math.random().toString(36).substring(2, numChars + 2);
-}
-
-function buttonMouseOver(e) {
-	const titleText = e.querySelector('span.btn-title');
-
-	if (buttonHoverOn) {
-		titleText.style.letterSpacing = hoverLetterSpacing;
+	buttonMouseEnter() {
+		throw new Error("This method should be overridden by subclasses");
+	}
+	
+	buttonMouseLeave() {
+		throw new Error("This method should be overridden by subclasses");
 	}
 }
 
-function buttonMouseOut(e) {
-	const titleText = e.querySelector('span.btn-title');
+class ButtonHoverEffect extends ColonistButton {
+	#extraSpace;
+	constructor(buttonObject, hoverLetterSpacing = "6.5px") {
+		super(buttonObject);
+		this.hoverLetterSpacing = hoverLetterSpacing;
+		this.hasHoverEffect = this.shouldEnableHoverEffect();
+		this.letterSpacingDifference = Math.abs(parseFloat(this.hoverLetterSpacing) - parseFloat(super.defaultLetterSpacing));
+		this.#extraSpace = this.letterSpacingDifference * super.numLetters;
+	}
 
-	if (titleText.style.letterSpacing === hoverLetterSpacing) {
-		titleText.style.letterSpacing = defaultLetterSpacing;
+	buttonAction() {
+        throw new Error("This method should be overridden by subclasses");
+    }
+
+	buttonMouseEnter() {
+		if (this.hasHoverEffect) {
+			this.titleText.style.letterSpacing = this.hoverLetterSpacing;
+		}
+	}
+	
+	buttonMouseLeave() {
+		this.titleText.style.letterSpacing = this.defaultLetterSpacing;
+	}
+
+	shouldEnableHoverEffect() {
+		return this.isLetterSpacingOnSameLine(this.titleText, this.buttonObject) && this.isLetterSpacingHalfContainerSize(titleText);
+	}
+
+	isLetterSpacingOnSameLine(textObject, textContainer) {
+		return Math.ceil(textObject.offsetWidth) + this.#extraSpace <= Math.floor(textContainer.offsetWidth);
+	}
+	
+	isLetterSpacingHalfScreenSize(textObject) {
+		return Math.ceil(textObject.offsetWidth) + this.#extraSpace < screen.width / 2 - bodyPaddingTotal;
+	}
+}
+
+class QuickPlayButton extends ButtonHoverEffect {
+	constructor(buttonObject) {
+		super(buttonObject);
+	}
+	buttonAction() {
+		window.location.href = "https://colonist.io/#" + this.generateAlphanumericRoomID();
+	}
+	
+	generateAlphanumericRoomID(roomIDLength = 4) {
+		return Math.random().toString(36).substring(2, roomIDLength + 2);
+	}
+}
+
+class PlayOnlineButton extends ButtonHoverEffect {
+	constructor(buttonObject) {
+		super(buttonObject);
+	}
+	async buttonAction() {
+		let apiResult = await this.getAPIResult();
+		console.log(apiResult);
+		let ids = apiResult.slice(0, 5).map(item => item.id);
+		console.log("First 5 room ids: " + ids);
+	}
+	
+	async getAPIResult() {
+		let apiResult = await this.sendAPIRequest().then(data => {
+			return data;
+		});
+		return apiResult.games;
+	}
+	
+	async sendAPIRequest() {
+		const url = 'https://colonist.io/api/game-list';
+	
+		return await fetch(url)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Error retrieving response from server.');
+				}
+				return response.json(); // This returns a promise
+			})
+			.catch(error => console.error('Error:', error));
 	}
 }
 
 // Function that determines whether the letter-spacing effect should take place on hover
-function setupHoverEffect() {
-	const bodyPaddingTotal = parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(document.body).getPropertyValue('padding-right'));
+// function setupHoverEffect() {
+// 	for (let i = 0; i < btns.length; i++) {
+// 		const titleText = btns[i].querySelector('span.btn-title');
+// 		const numLetters = titleText.innerText.length;
+// 		extraSpace = (letterSpacingDiffPix * (numLetters));
 
-	for (let i = 0; i < btns.length; i++) {
-		const titleText = btns[i].querySelector('span.btn-title');
-		const numLetters = titleText.innerText.length;
-		extraSpace = (letterSpacingDiffPix * (numLetters));
-
-		if (Math.ceil(titleText.offsetWidth) + extraSpace <= Math.floor(btns[i].offsetWidth) /* Ensure the increased lettering-space stays on the same line(s) (and does not switch from one line to two or vice versa) */
-			&& Math.ceil(titleText.offsetWidth) + extraSpace < screen.width / 2 - bodyPaddingTotal) { /* Ensure the increased lettering-space does not fill up more than half of the screen so other button stays the same size */
-			buttonHoverOn = true;
-		}
-		else {
-			buttonHoverOn = false;
-			break;
-		}
-	}
-}
+// 		if (isLetterSpacingOnSameLine(titleText, btns[i])
+// 			&& isLetterSpacingHalfContainerSize(titleText, screen)) { /* Ensure the increased lettering-space does not fill up more than half of the screen so other button stays the same size */
+// 			isButtonHoverEnabled = true;
+// 		}
+// 		else {
+// 			buttonHoverOn = false;
+// 			break;
+// 		}
+// 	}
+// }
 
 // Update letter-spacing effect capabilities
-window.addEventListener("resize", () => {
-	setupHoverEffect();
-});
+// window.addEventListener("resize", () => {
+// 	setupHoverEffect();
+// });
 
-window.onload = function () {
-	setupHoverEffect();
-};
+// window.onload = function () {
+// 	setupHoverEffect();
+// };
+
+const quickPlayButtonElement  = document.getElementById("quickPlayButton");
+const playOnlineButtonElement = document.getElementById("playOnlineButton");
+
+const buttonInstances = [new QuickPlayButton(quickPlayButtonElement), new PlayOnlineButton(playOnlineButtonElement)];
+
+Object.keys(buttonInstances).forEach(key => {
+	const buttonInstance = buttonInstances[key];
+	buttonInstance.buttonObject.addEventListener("click", function(event) {
+		buttonInstance.buttonAction();
+	});
+	buttonInstance.buttonObject.addEventListener("mouseenter", function(event) {
+		// console.log("mouseover");
+		buttonInstance.buttonMouseEnter();
+	});
+	buttonInstance.buttonObject.addEventListener("mouseleave", function(event) {
+		// console.log("mouseleave");
+		buttonInstance.buttonMouseLeave();
+	});
+  });
